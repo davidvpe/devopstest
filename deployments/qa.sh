c110384bf8d5942246dc153af30b0eca99e8c8d2
David Velarde <davidvpe@gmail.com>
2018-03-26 16:53:07 +0200

    Added flag to work

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,6 @@
 npm install -g now
 echo "deploying..."
-URL = $(now --docker -t NOW_TOKEN)
+URL = $(now --docker --public -t NOW_TOKEN)
 echo "running acceptance on... $URL"
 curl --silent -L $URL
 
