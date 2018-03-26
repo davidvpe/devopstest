eee66e572e643ed4b0f2283a2587507258aa4557
David Velarde <davidvpe@gmail.com>
2018-03-26 17:13:41 +0200

    porseacaso

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,7 +1,7 @@
 sudo npm install -g now
 echo "deploying..."
 echo "now --docker -t $NOW_TOKEN"
-URL=$(now --docker -t $NOW_TOKEN)
+$URL=$(now --docker -t $NOW_TOKEN)
 echo "running acceptance on... $URL"
 curl --silent -L $URL
 
