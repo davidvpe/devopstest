7812ac7c8abe9cc91345f61d22b220dee11693ad
David Velarde <davidvpe@gmail.com>
2018-03-26 17:18:27 +0200

    Intento nro 213123123125

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,6 @@
 sudo npm install -g --unsafe-perm now
 echo "deploying..."
-echo "now --docker -t $NOW_TOKEN"
+echo "now --docker --public -t $NOW_TOKEN"
 URL_NOW=$(now --docker -t $NOW_TOKEN)
 echo "running acceptance on... $URL_NOW"
 curl --silent -L $URL_NOW
