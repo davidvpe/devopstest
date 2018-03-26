9b7935dbac86b8cd7d6cc975f66048cb0a609f2a
David Velarde <davidvpe@gmail.com>
2018-03-26 17:11:03 +0200

    new intent

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,6 +1,7 @@
 sudo npm install -g now
 echo "deploying..."
-URL = $(now --docker -t $NOW_TOKEN)
+echo "now --docker -t $NOW_TOKEN"
+URL=$(now --docker -t $NOW_TOKEN)
 echo "running acceptance on... $URL"
 curl --silent -L $URL
 
