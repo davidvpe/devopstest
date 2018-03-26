b0e40efc76bb4ea7c83cbcf33173fdb6d2520ec4
David Velarde <davidvpe@gmail.com>
2018-03-26 17:16:46 +0200

    la ultimaaaaaa

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,9 +1,9 @@
-sudo npm install -g now
+sudo npm install -g --unsafe-perm now
 echo "deploying..."
 echo "now --docker -t $NOW_TOKEN"
-$URL=$(now --docker -t $NOW_TOKEN)
-echo "running acceptance on... $URL"
-curl --silent -L $URL
+URL_NOW=$(now --docker -t $NOW_TOKEN)
+echo "running acceptance on... $URL_NOW"
+curl --silent -L $URL_NOW
 
 
 
