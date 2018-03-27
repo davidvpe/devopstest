61148b2f74d1ccf360567231e82657f2fbead2d8
David Velarde <davidvpe@gmail.com>
2018-03-27 13:01:55 +0200

    yaaaa

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,10 +1,10 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.172604963.1433869664.1522083868-384145405.1522083868
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip?_ga=2.42370918.2146529047.1522086131-369822613.1522086131
-unzip /tmp/packer.zip -d ~
-unzip /tmp/terraform.zip -d ~
+unzip /tmp/packer.zip
+unzip /tmp/terraform.zip
 
-packer validate deployments/template.json &&
-packer build deployments/template.json &&
+./packer validate deployments/template.json &&
+./packer build deployments/template.json &&
 
 export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id") &&
 
