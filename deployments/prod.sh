c22c16de65f0408943e687e04f2282c4e6b71458
David Velarde <davidvpe@gmail.com>
2018-03-27 15:44:16 +0200

    no dots and git config

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,10 +1,10 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.172604963.1433869664.1522083868-384145405.1522083868
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip?_ga=2.42370918.2146529047.1522086131-369822613.1522086131
-unzip /tmp/packer.zip
-unzip /tmp/terraform.zip
+unzip /tmp/packer.zip -d /bin
+unzip /tmp/terraform.zip -d /bin
 
-./packer validate deployments/template.json &&
-./packer build deployments/template.json &&
+packer validate deployments/template.json &&
+packer build deployments/template.json &&
 
 export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"platzi-demo-$CIRCLE_BUILD_NUM\") | .id") &&
 
@@ -12,7 +12,9 @@ echo "Got the image id of the new digital ocean image" &&
 echo $TF_VAR_image_id &&
 
 cd infra && 
-../terraform init -input=false && 
-../terraform apply -input=false -auto-approve &&  cd .. &&
+terraform init -input=false && 
+terraform apply -input=false -auto-approve &&  cd .. &&
+git config --global user.email "circleci@awesomeMail.com" &&
+git config --global user.name "Circle CI Script" &&
 git add infra && git commit -m 'Deployed $CIRCLE_BUILD NUM [skip ci]' &&
 git push origin master
