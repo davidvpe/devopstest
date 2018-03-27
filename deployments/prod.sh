99beb2ee4321d7755af1b7112fc85e3c3602ee59
David Velarde <davidvpe@gmail.com>
2018-03-27 12:59:57 +0200

    sin carpeta bin

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,7 +1,7 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.2.1/packer_1.2.1_linux_amd64.zip?_ga=2.172604963.1433869664.1522083868-384145405.1522083868
 wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip?_ga=2.42370918.2146529047.1522086131-369822613.1522086131
-unzip /tmp/packer.zip -d ~/.local/bin
-unzip /tmp/terraform.zip -d ~/.local/bin
+unzip /tmp/packer.zip -d ~
+unzip /tmp/terraform.zip -d ~
 
 packer validate deployments/template.json &&
 packer build deployments/template.json &&
@@ -12,7 +12,7 @@ echo "Got the image id of the new digital ocean image" &&
 echo $TF_VAR_image_id &&
 
 cd infra && 
-terraform init -input=false && 
-terraform apply -input=false -auto-approve &&  cd .. &&
+../terraform init -input=false && 
+../terraform apply -input=false -auto-approve &&  cd .. &&
 git add infra && git commit -m 'Deployed $CIRCLE_BUILD NUM [skip ci]' &&
 git push origin master
